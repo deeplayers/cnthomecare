77128d1405c13771c549d612a28d14104a2fd320
amitesh <amitesitm2011@gmail.com>
2019-07-22 17:32:21 +0530

    add httpmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AboutComponent } from './about/about.component';
 import { ServiceComponent } from './service/service.component';
 import { HomesComponent } from './homes/homes.component';
-// import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { HttpClientModule } from '@angular/common/http';
+// httpClinetModule
+// Angular's preferred way to communicate with the server side is through the HttpClientModule module.
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,7 +20,7 @@ import { HomesComponent } from './homes/homes.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    // AngularFontAwesomeModule
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
