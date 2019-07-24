c189b5eacae318dd9133ea89a10bed39a8544933
amitesh <amitesitm2011@gmail.com>
2019-07-24 17:30:28 +0530

    change of nav and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { NavbarComponent } from './navbar/navbar.component';
 import { AboutComponent } from './about/about.component';
 import { ServiceComponent } from './service/service.component';
 import { HomesComponent } from './homes/homes.component';
 
 
 const routes: Routes = [
+  { path: '', component: HomesComponent},
   { path: 'About', component: AboutComponent },
-  { path: 'Home', component: HomesComponent}, 
   { path: 'Services', component: ServiceComponent },  
 
 ];
